1cf6b0d16bb53efd2973e25372122d0aab2e4172
tim minker <nigel8181@gmail.com>
2022-09-01 16:31:56 -0400

    added first component and simple main directory

--- the_game_room/src/App.js
+++ the_game_room/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Connect4 from './components/connect4.js'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [view, setView] = useState('main')
+
+
+  const connectView = () => {
+    if (view === 'main') {
+    setView('connect4')
+  } else {
+    setView('main')
+  }
+  }
+
+  useEffect(() => {
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {view === 'main' ?
+        <div>
+        <h1>Hello World!</h1>
+      <br/>
+      <p>Welcome to The Game Room! Where you can play all of your favorite (not all) games!</p>
+      <br/>
+      <h2>You can choose from the following games:</h2>
+      <ul>
+        <li onClick={connectView}>Connect 4</li>
+        <li>Tic Tac Toe</li>
+        <li>Checkers</li>
+      </ul>
+      </div>
+      : null}
+
+      {view === 'connect4' ?
+        <Connect4 connectView={connectView}/>
+      : null}
+    </>
   );
 }
 
---
+++ the_game_room/src/components/connect4.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Connect4 = (props) => {
+  return (
+    <>
+      <h1>Connect 4</h1>
+      <p>(game goes here)</p>
+      <button onClick={props.connectView}>Go Back</button>
+    </>
+  )
+}
+
+export default Connect4
