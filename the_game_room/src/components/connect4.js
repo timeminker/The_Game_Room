25727cb564f7325e101656074f66500ca03a987d
John Komninos <johnkomninos@johns-air.myfiosgateway.com>
2022-09-06 19:03:44 -0400

    built the connect 4 board

--- the_game_room/src/components/connect4.js
+++ the_game_room/src/components/connect4.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
+
+
 const Connect4 = (props) => {
   return (
     <>
-      <h1>Connect 4</h1>
-      <p>(game goes here)</p>
-      <button onClick={props.connectView}>Go Back</button>
+      <div class="Connect4cell">{props.connectNumbers}</div>
     </>
   )
 }
