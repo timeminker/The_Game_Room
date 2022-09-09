5262c6aeace93f372c3ab3b9399c526b3b071033
Matthew Downey <matthewdowney@Matthews-Air.fios-router.home>
2022-09-09 16:45:58 -0400

    added initial randomization function for memory game, commented out StrictMode

--- the_game_room/src/components/memory.js
+++ the_game_room/src/components/memory.js
@@ -1,18 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Memory = (props) => {
-  return (
-    <>
-        <h1>Memory</h1>
-        <div className='memory-board'>
-            <div className='memory-square' id='memory-square-1'></div>
-            <div className='memory-square' id='memory-square-2'></div>
-            <div className='memory-square' id='memory-square-3'></div>
-            <div className='memory-square' id='memory-square-4'></div>
-        </div>
-        <button onClick={props.connectView}>Go Back</button>
-    </>
-  )
+
+    const [colorShapeCombos, setColorShapeCombos] = useState([])
+
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
+    const shapes = ['circle', 'square', 'triangle', 'rectangle', 'horizontal-equals', 'vertical-equals']
+    const combos = []
+
+    const randomize = () => {
+        for (let i = 1; i <= 15; i++) {
+            const randomColorIndex = Math.floor(Math.random() * colors.length)
+            const randomShapeIndex = Math.floor(Math.random() * shapes.length)
+            if (combos[colors[randomColorIndex]] === shapes[randomShapeIndex]) {
+                i--
+            } else {
+                combos.push({[colors[randomColorIndex]]: shapes[randomShapeIndex]})
+            }
+        }
+        setColorShapeCombos(combos)
+    }
+
+    useEffect(() => {
+        randomize()
+    }, [])
+
+    return (
+        <>
+            <h1>Memory</h1>
+            <div className='memory-board'>
+                {colorShapeCombos.map((combo) => {
+                    return (
+                        <div className='memory-square' id={combo}></div>
+                    )
+                })}
+            </div>
+            <button onClick={props.connectView}>Go Back</button>
+        </>
+    )
 }
 
 export default Memory
