b987b48eafa1c869facdf82defc6e3df088c1bad
Matthew Downey <matthewdowney@Matthews-Air.fios-router.home>
2022-09-27 16:58:10 -0400

    added ability to overturn memory cards

--- the_game_room/src/components/memory.js
+++ the_game_room/src/components/memory.js
@@ -14,24 +14,31 @@ const Memory = (props) => {
         for (let i = 1; i <= 4; i++) {
             const randomColorIndex = Math.floor(Math.random() * colors.length)
             const randomShapeIndex = Math.floor(Math.random() * shapes.length)
-            const comboAlreadyExists = combosTemp.find(element => element[0] === colors[randomColorIndex] && element[1] === shapes[randomShapeIndex])
+            const comboAlreadyExists = combosTemp.find(element => element.color === colors[randomColorIndex] && element.shape === shapes[randomShapeIndex])
             if (comboAlreadyExists) {
-                console.log('already in there')
+                console.log('already exists')
                 i--
             } else {
-                console.log('not in there')
-                combosTemp.push([colors[randomColorIndex], shapes[randomShapeIndex]])
+                console.log('does not exist')
+                combosTemp.push({color: colors[randomColorIndex], shape: shapes[randomShapeIndex], overturned: false})
             }
         }
         for (let i = 0; i < 2; i++) {
             for (let tempCombo of combosTemp) {
-                combos.push(tempCombo)
+                combos.push({...tempCombo, index: combos.length})
             }
         }
         for (let i = 0; i < 5; i++) {
             combos.sort((a, b) => 0.5 - Math.random())
         }
         setColorShapeCombos(combos)
+    }
+
+    const handleClick = (comboToChange) => {
+        let index = colorShapeCombos.indexOf(comboToChange)
+        let temp = [...colorShapeCombos]
+        temp[index].overturned = true
+        setColorShapeCombos(temp)
     }
 
     useEffect(() => {
@@ -42,9 +49,11 @@ const Memory = (props) => {
         <>
             <h1>Memory</h1>
             <div className='memory-board'>
-                {colorShapeCombos.map((combo) => {
+                {colorShapeCombos?.map((combo) => {
                     return (
-                        <div className={'memory-square' + ' ' + combo[0] + " " + combo[1]}></div>
+                        combo.overturned === true?
+                            <div className={combo.color + " " + combo.shape}>{combo.shape}</div>
+                        : <div className='memory-square' onClick={() => handleClick(combo)}></div>
                     )
                 })}
             </div>
